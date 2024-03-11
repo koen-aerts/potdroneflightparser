4d49fc7c8798792d64e50a30d8e9c0eb280d1edc
Koen Aerts <knaerts@gmail.com>
2024-03-10 19:37:11 -0600

    Handle spec file for windows build.

--- builders/windows/build.bat
+++ builders/windows/build.bat
@@ -1,5 +1,6 @@
 rem Generate exe version file.
 create-version-file ./app-version.yml --outfile ../../src/file_version_info.txt
+copy FlightLogViewer.spec ..\..\src\
 cd ..\..\src
 del /f /q /s build
 del /f /q /s dist
@@ -10,4 +11,5 @@ rem pyinstaller --name FlightLogViewer -i ./assets/app-icon256.png --version-fil
 rem Run pyinstaller using the spec file.
 pyinstaller FlightLogViewer.spec
 
+del /f /q FlightLogViewer.spec
 cd ..\builders\windows
