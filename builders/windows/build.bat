ff6be806feb810013125858b7630223d0dd6a8eb
Koen Aerts <knaerts@gmail.com>
2024-03-13 18:35:37 -0600

    Updated windows build to include mapview path and automated packager.

--- builders/windows/build.bat
+++ builders/windows/build.bat
@@ -2,14 +2,22 @@ rem Generate exe version file.
 create-version-file ./app-version.yml --outfile ../../src/file_version_info.txt
 copy FlightLogViewer.spec ..\..\src\
 cd ..\..\src
-del /f /q /s build
-del /f /q /s dist
+rmdir /q /s build
+rmdir /q /s dist
 
-rem Create spec file.
-rem pyinstaller --name FlightLogViewer -i ./assets/app-icon256.png --version-file file_version_info.txt main.py
+if not exist kivy_garden\ (
+  curl -OL https://github.com/kivy-garden/mapview/archive/refs/tags/1.0.6.zip
+  tar -xf 1.0.6.zip
+  move mapview-1.0.6\kivy_garden .
+  del /f /q /s 1.0.6.zip
+  rmdir /q /s mapview-1.0.6
+  copy ..\builders\mapview_constants.py .\kivy_garden\mapview\constants.py
+)
 
 rem Run pyinstaller using the spec file.
 pyinstaller FlightLogViewer.spec
 
 del /f /q FlightLogViewer.spec
 cd ..\builders\windows
+
+"C:\Program Files (x86)\NSIS\makensis.exe" .\FlightLogViewer.nsi
