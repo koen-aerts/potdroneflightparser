3390ceb0c8f6531c6653a43ea762f8a390630f77
Koen Aerts <knaerts@gmail.com>
2024-05-21 09:12:24 -0600

    restored locale initialization

--- src/main.py
+++ src/main.py
@@ -1858,6 +1858,7 @@ class MainApp(MDApp):
     '''
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        locale.setlocale(locale.LC_ALL, '')
         self.is_desktop = platform in ('linux', 'win', 'macosx')
         self.is_ios = platform == 'ios'
         self.title = self.appTitle
