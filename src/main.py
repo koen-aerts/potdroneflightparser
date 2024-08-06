5a14fc113ff3180329f0d691a18c43eff9fea44d
Koen Aerts <knaerts@gmail.com>
2024-08-05 20:41:57 -0600

    Redunce number of frames for KML rendering.

--- src/main.py
+++ src/main.py
@@ -652,35 +652,29 @@ class MainApp(MDApp):
         doc = ET.SubElement(root, "Document")
         ET.SubElement(doc, "name").text = f"{self.root.ids.selected_model.text} logs of {self.root.ids.value_date.text}"
         ET.SubElement(doc, "description").append(ET.Comment(f' --><![CDATA[Logfile: {self.zipFilename}<br><br>Exported: {datetime.datetime.now().isoformat()}<br><br><a href="https://github.com/koen-aerts/potdroneflightparser">Flight Log Viewer</a>]]><!-- '))
-
         style = ET.SubElement(doc, "Style", id="pathStyle")
         lineStyle = ET.SubElement(style, "LineStyle")
         pathColor = self.assetColors[int(self.root.ids.selected_flight_path_color.value)]
         ET.SubElement(lineStyle, "color").text = f"ff{pathColor[5:7]}{pathColor[3:5]}{pathColor[1:3]}"
         ET.SubElement(lineStyle, "width").text = self.pathWidths[int(self.root.ids.selected_flight_path_width.value)]
-
         style = ET.SubElement(doc, "Style", id="homeStyle")
         iconStyle = ET.SubElement(style, "IconStyle")
         icon = ET.SubElement(iconStyle, "Icon")
         ET.SubElement(icon, "href").text = f"https://raw.githubusercontent.com/koen-aerts/potdroneflightparser/v2.2.0/src/assets/Home-{str(int(self.root.ids.selected_marker_home_color.value)+1)}.png"
         ET.SubElement(iconStyle, "hotSpot", x="0.5", y="0.5", xunits="fraction", yunits="fraction")
-
         style = ET.SubElement(doc, "Style", id="ctrlStyle")
         iconStyle = ET.SubElement(style, "IconStyle")
         icon = ET.SubElement(iconStyle, "Icon")
         ET.SubElement(icon, "href").text = f"https://raw.githubusercontent.com/koen-aerts/potdroneflightparser/v2.2.0/src/assets/Controller-{str(int(self.root.ids.selected_marker_ctrl_color.value)+1)}.png"
         ET.SubElement(iconStyle, "hotSpot", x="0.5", y="0.5", xunits="fraction", yunits="fraction")
-
         style = ET.SubElement(doc, "Style", id="droneStyle")
         iconStyle = ET.SubElement(style, "IconStyle")
         icon = ET.SubElement(iconStyle, "Icon")
         ET.SubElement(icon, "href").text = f"https://raw.githubusercontent.com/koen-aerts/potdroneflightparser/v2.2.0/src/assets/Drone-{str(int(self.root.ids.selected_marker_drone_color.value)+1)}.png"
         ET.SubElement(iconStyle, "hotSpot", x="0.5", y="0.5", xunits="fraction", yunits="fraction")
-
         style = ET.SubElement(doc, "Style", id="hidePoints")
         listStyle = ET.SubElement(style, "ListStyle")
         ET.SubElement(listStyle, "listItemType").text = "checkHideChildren"
-
         flightNo = 1
         while flightNo <= len(self.flightOptions):
             coords = ''
@@ -691,53 +685,59 @@ class MainApp(MDApp):
             ET.SubElement(folder, "description").append(ET.Comment(f' --><![CDATA[Duration: {str(self.flightStats[flightNo][3])}<br>Distance flown: {self.fmt_num(self.dist_val(self.flightStats[flightNo][9]))} {self.dist_unit()}]]><!-- '))
             ET.SubElement(folder, "styleUrl").text = "#hidePoints"
             ET.SubElement(folder, "visibility").text = "0"
+            prevtimestamp = None
+            maxelapsedms = d = datetime.timedelta(microseconds=500000)
             isfirstrow = True
             for rowIdx in range(self.currentStartIdx, self.currentEndIdx+1):
                 row = self.logdata[rowIdx]
-                thistimestamp = f"{datetime.datetime.fromisoformat(row[self.columns.index('timestamp')]).isoformat(sep='T', timespec='milliseconds')}Z"
-                dronelon = row[self.columns.index('dronelon')]
-                dronelat = row[self.columns.index('dronelat')]
-                dronealt = row[self.columns.index('altitude2metric')]
-                if isfirstrow:
-                    lookAt = ET.SubElement(folder, "LookAt")
-                    ET.SubElement(lookAt, "longitude").text = dronelon
-                    ET.SubElement(lookAt, "latitude").text = dronelat
-                    ET.SubElement(lookAt, "altitude").text = "200" # Viewing altitude
-                    ET.SubElement(lookAt, "heading").text = "0"
-                    ET.SubElement(lookAt, "tilt").text = "45"
-                    ET.SubElement(lookAt, "range").text = str((self.flightStats[flightNo][0]*2)+500) # Determine potential good viewing distance.
-                    ET.SubElement(lookAt, "altitudeMode").text = "relativeToGround"
-                    isfirstrow = False
-                placeMark = ET.SubElement(folder, "Placemark")
-                timest = ET.SubElement(placeMark, "TimeStamp")
-                ET.SubElement(timest, "when").text = thistimestamp
-                ET.SubElement(placeMark, "styleUrl").text = "#droneStyle"
-                point = ET.SubElement(placeMark, "Point")
-                ET.SubElement(point, "altitudeMode").text = "relativeToGround"
-                ET.SubElement(point, "coordinates").text = f"{dronelon},{dronelat},{dronealt}"
-                if (len(coords) > 0):
-                    coords += '\n'
-                coords += f"{dronelon},{dronelat},{dronealt}"
-                homelon = row[self.columns.index('homelon')]
-                homelat = row[self.columns.index('homelat')]
-                if homelon != '0.0' and homelat != '0.0':
-                    placeMark = ET.SubElement(folder, "Placemark")
+                thistimestamp = datetime.datetime.fromisoformat(row[self.columns.index('timestamp')])
+                elapsedFrame = None if prevtimestamp is None else thistimestamp - prevtimestamp # elasped microseconds since last frame.
+                if elapsedFrame is None or elapsedFrame > maxelapsedms: # Omit frames that are within maxelapsedms microseconds from each other.
+                    timestampstr = f"{thistimestamp.isoformat(sep='T', timespec='milliseconds')}Z"
+                    dronelon = row[self.columns.index('dronelon')]
+                    dronelat = row[self.columns.index('dronelat')]
+                    dronealt = row[self.columns.index('altitude2metric')] # KML uses metric units.
+                    if isfirstrow:
+                        lookAt = ET.SubElement(folder, "LookAt")
+                        ET.SubElement(lookAt, "longitude").text = dronelon
+                        ET.SubElement(lookAt, "latitude").text = dronelat
+                        ET.SubElement(lookAt, "altitude").text = "200" # Viewing altitude
+                        ET.SubElement(lookAt, "heading").text = "0" # Look North
+                        ET.SubElement(lookAt, "tilt").text = "45" # Look down 45 degrees
+                        ET.SubElement(lookAt, "range").text = str((self.flightStats[flightNo][0]*2)+500) # Determine potential good viewing distance.
+                        ET.SubElement(lookAt, "altitudeMode").text = "relativeToGround"
+                        isfirstrow = False
+                    placeMark = ET.SubElement(folder, "Placemark") # Drone marker.
                     timest = ET.SubElement(placeMark, "TimeStamp")
-                    ET.SubElement(timest, "when").text = thistimestamp
-                    ET.SubElement(placeMark, "styleUrl").text = "#homeStyle"
+                    ET.SubElement(timest, "when").text = timestampstr
+                    ET.SubElement(placeMark, "styleUrl").text = "#droneStyle"
                     point = ET.SubElement(placeMark, "Point")
                     ET.SubElement(point, "altitudeMode").text = "relativeToGround"
-                    ET.SubElement(point, "coordinates").text = f"{homelon},{homelat},0"
-                ctrllon = row[self.columns.index('ctrllon')]
-                ctrllat = row[self.columns.index('ctrllat')]
-                if ctrllon != '0.0' and ctrllat != '0.0':
-                    placeMark = ET.SubElement(folder, "Placemark")
-                    timest = ET.SubElement(placeMark, "TimeStamp")
-                    ET.SubElement(timest, "when").text = thistimestamp
-                    ET.SubElement(placeMark, "styleUrl").text = "#ctrlStyle"
-                    point = ET.SubElement(placeMark, "Point")
-                    ET.SubElement(point, "altitudeMode").text = "relativeToGround"
-                    ET.SubElement(point, "coordinates").text = f"{ctrllon},{ctrllat},1.5" # Assume average human holds controller 1.5 meters from ground.
+                    ET.SubElement(point, "coordinates").text = f"{dronelon},{dronelat},{dronealt}"
+                    if (len(coords) > 0):
+                        coords += '\n'
+                    coords += f"{dronelon},{dronelat},{dronealt}" # flight path coordinates and altitude.
+                    homelon = row[self.columns.index('homelon')]
+                    homelat = row[self.columns.index('homelat')]
+                    if homelon != '0.0' and homelat != '0.0': # Home marker.
+                        placeMark = ET.SubElement(folder, "Placemark")
+                        timest = ET.SubElement(placeMark, "TimeStamp")
+                        ET.SubElement(timest, "when").text = timestampstr
+                        ET.SubElement(placeMark, "styleUrl").text = "#homeStyle"
+                        point = ET.SubElement(placeMark, "Point")
+                        ET.SubElement(point, "altitudeMode").text = "relativeToGround"
+                        ET.SubElement(point, "coordinates").text = f"{homelon},{homelat},0"
+                    ctrllon = row[self.columns.index('ctrllon')]
+                    ctrllat = row[self.columns.index('ctrllat')]
+                    if ctrllon != '0.0' and ctrllat != '0.0': # Controller marker.
+                        placeMark = ET.SubElement(folder, "Placemark")
+                        timest = ET.SubElement(placeMark, "TimeStamp")
+                        ET.SubElement(timest, "when").text = timestampstr
+                        ET.SubElement(placeMark, "styleUrl").text = "#ctrlStyle"
+                        point = ET.SubElement(placeMark, "Point")
+                        ET.SubElement(point, "altitudeMode").text = "relativeToGround"
+                        ET.SubElement(point, "coordinates").text = f"{ctrllon},{ctrllat},1.5" # Assume average human holds controller 1.5 meters from ground.
+                    prevtimestamp = thistimestamp
             placeMark = ET.SubElement(folder, "Placemark")
             ET.SubElement(placeMark, "name").text = f"Flight Path {flightNo}"
             ET.SubElement(placeMark, "styleUrl").text = "#pathStyle"
